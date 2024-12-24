39280852f23267d883faed8e26f1312f1603cd65
Aslı <aslibyr00@gmail.com>
2024-12-24 14:59:34 +0300

    Image View refactor

--- app/src/main/java/com/aslibayar/foody/components/image_view/ImageView.kt
+++ app/src/main/java/com/aslibayar/foody/components/image_view/ImageView.kt
@@ -2,14 +2,9 @@ package com.aslibayar.foody.components.image_view
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -45,25 +40,11 @@ fun CustomImageView(
         imageLoader = imageLoader,
         contentDescription = "",
         loading = {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.White),
-                contentAlignment = Alignment.Center
-            ) {
-                CircularProgressIndicator(color = Orange)
-            }
+            CircularProgressIndicator(color = Orange)
         },
         modifier = modifier,
         error = {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color(0xFFF6F6F9)),
-                contentAlignment = Alignment.Center
-            ) {
-                Image(painter = painterResource(id = errorResource), contentDescription = "error")
-            }
+            Image(painter = painterResource(id = errorResource), contentDescription = "error")
         },
         contentScale = contentScale
     )
