21552e0b83d9ce64adadee630145cf4b06851cba
Aslı <aslibyr00@gmail.com>
2025-01-17 17:59:30 +0300

    Update NetworkStatusScreen to handle network status changes more effectively.

--- app/src/main/java/com/aslibayar/foody/ui/common/NetworkStatusScreen.kt
+++ app/src/main/java/com/aslibayar/foody/ui/common/NetworkStatusScreen.kt
@@ -6,20 +6,21 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 import com.aslibayar.foody.ui.theme.CustomTextStyle
-import com.aslibayar.foody.ui.theme.Orange
 
 @Composable
-fun NetworkStatusScreen(
-    onRetryClick: () -> Unit
-) {
+fun NetworkStatusScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -27,16 +28,18 @@ fun NetworkStatusScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "No Internet Connection",
-            style = CustomTextStyle.regularBlackMedium,
+        Icon(
+            imageVector = Icons.Default.Refresh,
+            contentDescription = "No Internet Connection",
+            tint = Color.Gray,
+            modifier = Modifier.size(50.dp)
         )
         Spacer(modifier = Modifier.height(16.dp))
-        Button(
-            onClick = { onRetryClick() },
-            colors = ButtonDefaults.buttonColors(containerColor = Orange)
-        ) {
-            Text("Retry")
-        }
+        Text(
+            text = "No Internet Connection",
+            fontStyle = FontStyle.Italic,
+            style = CustomTextStyle.regularBlackMedium,
+            color = Color.Gray
+        )
     }
 }
